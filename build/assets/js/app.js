a7e3ddf22af3566f9e1fbe60e96d33f57bd233e6
Your Name <you@example.com>
2019-02-17 14:25:33 +0300

    add build

---
+++ build/assets/js/app.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function() {
+    $('.nav__icon').on('click', function() {
+      $(this).closest('.nav').toggleClass('menu_state_open');
+      
+    });
+  });
+})(jQuery);
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIihmdW5jdGlvbigkKXtcbiAgJChmdW5jdGlvbigpIHtcbiAgICAkKCcubmF2X19pY29uJykub24oJ2NsaWNrJywgZnVuY3Rpb24oKSB7XG4gICAgICAkKHRoaXMpLmNsb3Nlc3QoJy5uYXYnKS50b2dnbGVDbGFzcygnbWVudV9zdGF0ZV9vcGVuJyk7XG4gICAgICBcbiAgICB9KTtcbiAgfSk7XG59KShqUXVlcnkpOyJdfQ==
